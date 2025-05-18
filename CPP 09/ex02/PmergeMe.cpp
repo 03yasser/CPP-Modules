6f35b2c6399e40c980648a067a7c607b9d9a4e1f
Yasser Boutslighoi <yboutsli@e2r13p5.1337.ma>
2025-05-18 18:22:48 +0100

    update

---
+++ CPP 09/ex02/PmergeMe.cpp
@@ -0,0 +1,203 @@
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe(void) {}
+
+PmergeMe::PmergeMe(const PmergeMe &copy)
+{
+	*this = copy;
+}
+
+PmergeMe::~PmergeMe(void) {}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe &other)
+{
+	if (this != &other)
+	{
+		this->vec = other.vec;
+		this->deq = other.deq;
+	}
+	return (*this);
+}
+
+void PmergeMe::printBefore(void)
+{
+	std::cout << "Before: ";
+	for (int i = 0; i < static_cast<int>(vec.size()); ++i)
+	{
+		std::cout << vec[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::checkInput(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		throw std::invalid_argument("Error: No arguments provided.");
+	}
+	for (int i = 1; i < ac; ++i)
+	{
+		if (av[i][0] == '-')
+		{
+			throw std::invalid_argument("Error: Negative numbers are not allowed.");
+		}
+		if (av[i][0] == '\0')
+		{
+			throw std::invalid_argument("Error: Empty argument provided.");
+		}
+		for (int j = 0; av[i][j] != '\0'; ++j)
+		{
+			if (!isdigit(av[i][j]))
+			{
+				throw std::invalid_argument("Error: Invalid character in input.");
+			}
+		}
+		std::stringstream ss(av[i]);
+		unsigned int num;
+		ss >> num;
+		if (num > 2147483647)
+		{
+			throw std::invalid_argument("Error: Number out of range.");
+		}
+		vec.push_back(num);
+		deq.push_back(num);
+	}
+}
+
+
+void PmergeMe::run(void)
+{
+	printBefore();
+	sortVector();
+
+}
+
+static void sort_vector_pairs(std::vector<std::pair<int, int> > &vecPairs)
+{
+	for (size_t i = 1; i < vecPairs.size(); ++i)
+	{
+		while (i > 0 && vecPairs[i].first < vecPairs[i - 1].first)
+		{
+			std::swap(vecPairs[i], vecPairs[i - 1]);
+			--i;
+		}
+	}
+}
+
+template <typename T>
+void jacobsthal(T &c, size_t n)
+{
+	T v;
+
+	v.push_back(0);
+	v.push_back(1);
+	for (size_t i = 2; i < n + 2; i++)
+	{
+		v.push_back(v[i - 1] + 2 * v[i - 2]);
+	}
+	for (size_t i = 2; i < n + 2; i++)
+	{
+		c.push_back(v[i]);
+	}
+}
+
+void PmergeMe::sortVector(void)
+{
+	std::vector<std::pair<int, int> > vecPairs;
+	std::vector<int> jacobsthalVec;
+	int isodd = 0;
+	size_t size = vec.size();
+	int unpaired;
+	if (size % 2 != 0)
+	{
+		isodd = 1;
+		unpaired = vec[size - 1];
+		size--;
+		vec.pop_back();
+	}
+	for (size_t i = 0; i < size - 1; i += 2)
+	{
+		vecPairs.push_back(std::make_pair(vec[i], vec[i + 1]));
+		if (vecPairs.back().first < vecPairs.back().second)
+		{
+			std::swap(vecPairs.back().first, vecPairs.back().second);
+		}
+	}
+	// std::cout << "before: ";
+	// for (size_t i = 0; i < vecPairs.size(); ++i)
+	// {
+	// 	std::cout << "(" << vecPairs[i].first << ", " << vecPairs[i].second << ") ";
+	// }
+	// if (isodd)
+	// {
+	// 	std::cout <<'(' <<  unpaired << ')' << std::endl;
+	// }
+	sort_vector_pairs(vecPairs);
+	std::vector<int> mainVec, pendVec;
+	mainVec.push_back(vecPairs[0].second);
+	for (size_t i = 0 ; i < vecPairs.size(); ++i)
+	{
+		mainVec.push_back(vecPairs[i].first);
+		if (i < vecPairs.size() - 1)
+		{
+			pendVec.push_back(vecPairs[i + 1].second);
+		}
+	}
+	// std::cout << "main: ";
+	// for (size_t i = 0; i < mainVec.size(); ++i)
+	// {
+	// 	std::cout << mainVec[i] << " ";
+	// }
+	// std::cout << std::endl;
+	std::cout << "pend: ";
+	for (size_t i = 0; i < pendVec.size(); ++i)
+	{
+		std::cout << pendVec[i] << " ";
+	}
+	std::cout << std::endl;
+	mainVec.reserve(size + 1);
+	jacobsthal(jacobsthalVec, pendVec.size());
+	// for (size_t i = 0; i < jacobsthalVec.size(); i++)
+	// {
+	// 	std::cout << jacobsthalVec[i] << " | ";
+	// }
+	// std::cout << std::endl;
+	size_t Jacob_index = 0;
+	for (size_t Pend_index = 0; Pend_index < pendVec.size(); Pend_index++)
+	{
+		size_t index = jacobsthalVec[Jacob_index];
+		
+		if (index <= pendVec.size())
+		{
+			int value = pendVec[index - 1];
+			
+			std::vector<int>::iterator pos = std::upper_bound(mainVec.begin(),mainVec.end(),value);
+			mainVec.insert(pos, value);
+			pendVec[index -1] = -1;
+			Jacob_index++;
+		}
+		else
+			break;
+	}
+	for (size_t i = 0; i < pendVec.size(); ++i)
+	{
+		if (pendVec[i] != -1)
+		{
+			std::vector<int>::iterator pos = std::upper_bound(mainVec.begin(), mainVec.end(), pendVec[i]);
+			mainVec.insert(pos, pendVec[i]);
+		}
+	}
+	if (isodd)
+	{
+		std::vector<int>::iterator pos;
+		pos = std::upper_bound(mainVec.begin(), mainVec.end(),unpaired);
+		mainVec.insert(pos, unpaired);
+	}
+	vec = mainVec;
+	std::cout << "After: ";
+	for (size_t i = 0; i < vec.size(); ++i)
+	{
+		std::cout << vec[i] << " ";
+	}
+	std::cout << std::endl;
+}
